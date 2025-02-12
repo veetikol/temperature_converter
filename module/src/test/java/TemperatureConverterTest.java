4ebe01976bc9d6fe24b1c5b19fe95a550a6b28e8
veetikol <vedek03@gmail.com>
2025-02-12 15:15:31 +0200

    init

---
+++ module/src/test/java/TemperatureConverterTest.java
@@ -0,0 +1,29 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TemperatureConverterTest {
+
+    TemperatureConverter converter = new TemperatureConverter();
+
+    @Test
+    public void testFahrenheitToCelsius() {
+        assertEquals(0, converter.fahrenheitToCelsius(32), 0.01);
+        assertEquals(100, converter.fahrenheitToCelsius(212), 0.01);
+        assertEquals(-40, converter.fahrenheitToCelsius(-40), 0.01);
+    }
+
+    @Test
+    public void testCelsiusToFahrenheit() {
+        assertEquals(32, converter.celsiusToFahrenheit(0), 0.01);
+        assertEquals(212, converter.celsiusToFahrenheit(100), 0.01);
+        assertEquals(-40, converter.celsiusToFahrenheit(-40), 0.01);
+    }
+
+    @Test
+    public void testIsExtremeTemperature() {
+        assertTrue(converter.isExtremeTemperature(-50));
+        assertFalse(converter.isExtremeTemperature(20));
+        assertTrue(converter.isExtremeTemperature(60));
+    }
+}
